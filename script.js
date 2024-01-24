d58b4b695dc1a5b4443ab434fc7a3d85ccce0c2d
Josiah Viernes <viernesjosiah6@gmail.com>
2024-01-24 18:29:13 +0800

    Added: Player and Computer Selection Function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const playButton = document.getElementById("play-game")
 const gameState = document.getElementById("game-container")
-const homeState = document.getElementById("home-container")
+const result = document.getElementById("output")
+const playerSelect = document.getElementById("player-select")
+const computerSelect = document.getElementById("computer-select")
+const gameTitle =  document.getElementById("game-title")
+const selectionButtons = document.querySelectorAll(".choices")
 
-//Play Function
+
+//Play Game Function
 playButton.addEventListener("click", ()=>{
     let hiddenItems = document.querySelectorAll(".hide")
 
@@ -11,6 +16,32 @@ playButton.addEventListener("click", ()=>{
     })
 
     playButton.classList.add("hide")
+    selectionButtons.forEach(button => {
+        button.classList.add("in-game")
+    })
+    gameTitle.classList.add("in-game")
 
 })
+
+//Selection Button Functions
+selectionButtons.forEach(selection => {
+    selection.addEventListener("click", () => {
+        const playerChoice = selection.getAttribute("choice-data")
+        let computerChoice = comChoice()
+        playerSelect.src = `/img/${playerChoice}.png`
+        computerSelect.src = `/img/${computerChoice}.png`
+        console.log(computerChoice)
+        
+    })
+})
+
+//Computer Selection
+const comChoice  = () =>{
+    const choices = ["rock", "paper", "scissor"]
+    const randomSelection = Math.floor(Math.random() * choices.length)
+
+    return choices[randomSelection]
+}
+
+
 
